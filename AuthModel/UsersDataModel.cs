38b4e2860e110f3af208c60f1b44d7acaff83540
Буляков Данил Радисович <bulyakov98@gmail.com>
2018-01-28 16:34:34 +0500

    Added empty EF model

---
+++ AuthModel/UsersDataModel.cs
@@ -0,0 +1,31 @@
+namespace AuthModel
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class UsersDataModel : DbContext
+    {
+        // Your context has been configured to use a 'UsersDataModel' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'AuthModel.UsersDataModel' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'UsersDataModel' 
+        // connection string in the application configuration file.
+        public UsersDataModel()
+            : base("name=UsersDataModel")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
